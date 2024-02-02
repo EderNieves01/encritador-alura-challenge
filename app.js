4dd7031bb26afb6ea425ad58e88c7b03c8069ec7
EderNieves01 <edernieves09@gmail.com>
2024-02-02 11:38:56 -0400

    probando los display

---
+++ app.js
@@ -0,0 +1,6 @@
+let divNoResultado = document.querySelector('.no-resultado');
+let divResultado = document.querySelector('.div-resultado');
+
+divResultado.style.display = "none";
+divNoResultado.style.display = "block";
+console.log(divResultado)
