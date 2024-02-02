8666c840f9864315515d65613878f3c3cef4d314
EderNieves01 <edernieves09@gmail.com>
2024-02-02 17:54:19 -0400

    probando botones

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//capturando botones
+
+const btnEncriptar = document.querySelector('#btnEncriptar');
+const btnDesencriptar = document.querySelector('#btnDesencriptar');
+
+btnEncriptar.addEventListener('click', probando);
+
+function probando(){
+    console.log("entrando")
+}
 let divNoResultado = document.querySelector('.no-resultado');
 let divResultado = document.querySelector('.div-resultado');
 
