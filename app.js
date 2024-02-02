fb2f819d9ac6a560aeb4730f1804f4e98ac08304
EderNieves01 <edernieves09@gmail.com>
2024-02-02 17:59:17 -0400

    comments

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 //capturando botones
-
 const btnEncriptar = document.querySelector('#btnEncriptar');
 const btnDesencriptar = document.querySelector('#btnDesencriptar');
+
+//capturando text area
 const textArea = document.querySelector('#textArea');
+
+//capturando divs
+let divNoResultado = document.querySelector('.no-resultado');
+let divResultado = document.querySelector('.div-resultado');
+
+
 
 btnEncriptar.addEventListener('click', probando);
 
 function probando(){
     console.log(textArea.value)
 }
-let divNoResultado = document.querySelector('.no-resultado');
-let divResultado = document.querySelector('.div-resultado');
+
 
 divResultado.style.display = "none";
 divNoResultado.style.display = "block";
