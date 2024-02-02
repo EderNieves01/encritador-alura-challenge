b833806ad3d25fd49ba85530f9cfbc04ba5a295b
EderNieves01 <edernieves09@gmail.com>
2024-02-02 17:57:25 -0400

    capturando textarea

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 const btnEncriptar = document.querySelector('#btnEncriptar');
 const btnDesencriptar = document.querySelector('#btnDesencriptar');
+const textArea = document.querySelector('#textArea');
 
 btnEncriptar.addEventListener('click', probando);
 
 function probando(){
-    console.log("entrando")
+    console.log(textArea.value)
 }
 let divNoResultado = document.querySelector('.no-resultado');
 let divResultado = document.querySelector('.div-resultado');
