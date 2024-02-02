788f213eeb504d90e620ca53354024e8368cfb31
EderNieves01 <edernieves09@gmail.com>
2024-02-02 18:12:16 -0400

    probando function encriptar

--- app.js
+++ app.js
@@ -6,18 +6,24 @@ const btnDesencriptar = document.querySelector('#btnDesencriptar');
 const textArea = document.querySelector('#textArea');
 
 //capturando divs
-let divNoResultado = document.querySelector('.no-resultado');
-let divResultado = document.querySelector('.div-resultado');
+// let divNoResultado = document.querySelector('.no-resultado');
+// let divResultado = document.querySelector('.div-resultado');
 
 
 
-btnEncriptar.addEventListener('click', probando);
+btnEncriptar.addEventListener('click', encriptar);
 
-function probando(){
-    console.log(textArea.value)
+function encriptar(){
+    const textArea = document.querySelector('#textArea');
+    let p = document.querySelector('#textEncriptado');
+    let divNoResultado = document.querySelector('.no-resultado');
+    let divResultado = document.querySelector('.div-resultado');
+    divNoResultado.style.display = "none";
+    divResultado.style.display = "block";
+    p.textContent = textArea.value;
 }
 
 
-divResultado.style.display = "none";
-divNoResultado.style.display = "block";
-console.log(divResultado)
+// divResultado.style.display = "none";
+// divNoResultado.style.display = "block";
+// console.log(divResultado)
